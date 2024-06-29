f68372d834b0a46ed6d60bada686151b4a5e70e4
maslomeister <maslomeister@gmail.com>
2024-06-29 11:43:39 +0300

    test statistics fetch

---
+++ defaults/python/main.test.py
@@ -0,0 +1,59 @@
+# credits to https://github.com/ma3a/SDH-PlayTime
+
+from collections import defaultdict
+from datetime import datetime, timedelta
+import time
+import asyncio
+import logging
+import os
+import sys
+from pathlib import Path
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from python.statistics import Statistics
+from python.db.sqlite_db import SqlLiteDb
+from python.db.dao import Dao
+from python.db.migration import DbMigration
+from python.usage_tracking import UsageTracking
+
+cwd = os.getcwd()
+
+
+def insert_element(data, index, element):
+    data.insert(index, element)
+
+
+class Plugin:
+    usage_tracking = None
+    statistics = None
+    game_id = None
+    game_name = None
+
+    async def _main(self):
+        try:
+            self.game_id = "Unknown"
+            self.game_name = "Steam"
+            db = SqlLiteDb(f"{cwd}/battery-usage.db")
+            # migration = DbMigration(db)
+            # migration.migrate()
+
+            dao = Dao(db)
+            self.statistics = Statistics(dao)
+
+            self.usage_tracking = UsageTracking(dao)
+
+        except Exception as e:
+            print(f"Error: {e}")
+
+    async def hourly_statistics(self):
+        try:
+            return await self.statistics.hourly_battery_usage_statistics()
+        except Exception as e:
+            print(f"Error: {e}")
+
+
+if __name__ == "__main__":
+    plugin = Plugin()
+    asyncio.run(plugin._main())
+    asyncio.run(plugin.hourly_statistics())
