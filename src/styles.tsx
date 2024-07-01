a095c56c58a4c45bec85308d5ab99fdd2def13a7
Nikita Maslov <maslomeister@gmail.com>
2024-07-01 14:03:59 +0300

    cleanup

--- src/styles.tsx
+++ src/styles.tsx
@@ -2,8 +2,8 @@ import { CSSProperties } from "react";
 import { FocusableExt } from "./components/FocusableExt";
 
 export const BLUE_COLOR = "#1A9FFF";
-// const DARK_GREY = "#4c4c4c";
-// const DEFAULT_BORDER_RADIUS = "2px";
+export const GREEN = "#00da2c";
+export const DESATURED_GREEN = "#01da2c65";
 
 export const pager_container: CSSProperties = {
   display: "flex",
@@ -18,6 +18,14 @@ export const vertical_container: CSSProperties = {
   alignContent: "space-between",
 };
 
+export const HorizontalContainerCSS = {
+  horizontal__container: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+  } as CSSProperties,
+};
+
 export const focus_panel_no_padding: CSSProperties = {
   padding: "0px 0px",
 };
@@ -27,4 +35,12 @@ export const hide_text_on_overflow: CSSProperties = {
   overflow: "hidden",
   width: "100%",
   whiteSpace: "nowrap",
+};
+
+export const VerticalContainerCSS = {
+  vertical__container: {
+    display: "flex",
+    flexDirection: "column",
+    alignContent: "space-between",
+  } as CSSProperties,
 };
