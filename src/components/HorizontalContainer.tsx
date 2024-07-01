d8e0bf0b953c06830ddd59011ae4026f792eb8c1
Nikita Maslov <maslomeister@gmail.com>
2024-07-01 14:17:53 +0300

    feat: horizontal container

---
+++ src/components/HorizontalContainer.tsx
@@ -0,0 +1,9 @@
+import { HorizontalContainerCSS } from "../styles";
+
+export const HorizontalContainer: React.FC<{}> = (props) => {
+  return (
+    <div style={HorizontalContainerCSS.horizontal__container}>
+      {props.children}
+    </div>
+  );
+};
