13f9a1034ecccced8f000789aef5b684ce8990c6
Nikita Maslov <maslomeister@gmail.com>
2024-07-02 09:04:10 +0300

    new test data

--- storybook/src/UsageGraph/index.tsx
+++ storybook/src/UsageGraph/index.tsx
@@ -11,119 +11,31 @@ export type UsageGraphProps = {
 };
 
 const data = [
-  {
-    hour: 12,
-    capacity: 20,
-    charging: 0,
-  },
-  {
-    hour: 13,
-    capacity: 20,
-    charging: 0,
-  },
-  {
-    hour: 14,
-    capacity: 25,
-    charging: 0,
-  },
-  {
-    hour: 15,
-    capacity: 26,
-    charging: 100,
-  },
-  {
-    hour: 16,
-    capacity: 30,
-    charging: 100,
-  },
-  {
-    hour: 17,
-    capacity: 32,
-    charging: 100,
-  },
-  {
-    hour: 18,
-    capacity: 34,
-    charging: 100,
-  },
-  {
-    hour: 19,
-    capacity: 38,
-    charging: 100,
-  },
-  {
-    hour: 20,
-    capacity: 40,
-    charging: 100,
-  },
-  {
-    hour: 21,
-    capacity: 45,
-    charging: 100,
-  },
-  {
-    hour: 22,
-    capacity: 50,
-    charging: 100,
-  },
-  {
-    hour: 23,
-    capacity: 55,
-    charging: 100,
-  },
-
-  {
-    hour: 0,
-    capacity: 60,
-    charging: 100,
-  },
-  {
-    hour: 1,
-    capacity: 65,
-    charging: 100,
-  },
-  {
-    hour: 2,
-    capacity: 70,
-    charging: 100,
-  },
-  {
-    hour: 3,
-    capacity: 75,
-    charging: 100,
-  },
-  {
-    hour: 4,
-    capacity: 80,
-    charging: 100,
-  },
-  {
-    hour: 5,
-    capacity: 85,
-    charging: 100,
-  },
-  {
-    hour: 6,
-    capacity: 86,
-    charging: 100,
-  },
-  {
-    hour: 7,
-    capacity: 88,
-    charging: 100,
-  },
-  {
-    hour: 8,
-    capacity: 88,
-    charging: 100,
-  },
-  {
-    hour: 9,
-    capacity: 90,
-    charging: 100,
-  },
+  { hour: "09", capacity: 93, charging: 100 },
+  { hour: "10", capacity: 97, charging: 100 },
+  { hour: "11", capacity: 100, charging: 100 },
+  { hour: "12", capacity: 99, charging: 0 },
+  { hour: "13", capacity: 99, charging: 0 },
+  { hour: "14", capacity: 99, charging: 0 },
+  { hour: "15", capacity: 99, charging: 0 },
+  { hour: "16", capacity: 99, charging: 0 },
+  { hour: "17", capacity: 99, charging: 0 },
+  { hour: "18", capacity: 99, charging: 0 },
+  { hour: "19", capacity: 96, charging: 0 },
+  { hour: "20", capacity: 92, charging: 0 },
+  { hour: "21", capacity: 89, charging: 0 },
+  { hour: "22", capacity: 85, charging: 0 },
+  { hour: "23", capacity: 82, charging: 0 },
+  { hour: "00", capacity: 78, charging: 0 },
+  { hour: "01", capacity: 75, charging: 0 },
+  { hour: "02", capacity: 71, charging: 0 },
+  { hour: "03", capacity: 68, charging: 0 },
+  { hour: "04", capacity: 39, charging: 0 },
+  { hour: "05", capacity: 30, charging: 100 },
+  { hour: "06", capacity: 61, charging: 100 },
+  { hour: "07", capacity: 90, charging: 100 },
+  { hour: "08", capacity: 98, charging: 100 },
 ];
-
 const small_data = [
   {
     hour: 8,
