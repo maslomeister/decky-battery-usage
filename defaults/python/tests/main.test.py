49d94d1830dd4eb1aa53b4fdd7dca1e2e4f27297
Nikita Maslov <maslomeister@gmail.com>
2024-07-04 09:30:56 +0300

    feat: format exception and move to tests

--- defaults/python/tests/main.test.py
+++ defaults/python/tests/main.test.py
@@ -8,20 +8,43 @@ import logging
 import os
 import sys
 from pathlib import Path
+import traceback
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
+
+cwd = os.getcwd()
 
 from python.statistics import Statistics
 from python.db.sqlite_db import SqlLiteDb
 from python.db.dao import Dao
-from python.db.migration import DbMigration
 from python.usage_tracking import UsageTracking
 
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
+
+
 cwd = os.getcwd()
+
+print(cwd)
 
 
 def insert_element(data, index, element):
     data.insert(index, element)
+
+
+def format_exception(e):
+    exception_list = traceback.format_stack()
+    exception_list = exception_list[:-2]
+    exception_list.extend(traceback.format_tb(sys.exc_info()[2]))
+    exception_list.extend(
+        traceback.format_exception_only(sys.exc_info()[0], sys.exc_info()[1])
+    )
+
+    exception_str = "Traceback (most recent call last):\n"
+    exception_str += "".join(exception_list)
+    # Removing the last \n
+    exception_str = exception_str[:-1]
+
+    return exception_str
 
 
 class Plugin:
@@ -53,7 +76,9 @@ class Plugin:
             for game in result["games_stats"]["games"]:
                 print(f"{game['game_name']} - {game['percentage']}% - {game["hours"]}")
 
-            print(f"SUSPENDED - {result["games_stats"]["suspended"]["percentage"]}%")
+            print(
+                f"SUSPENDED - {result["games_stats"]["suspended"]["percentage"]}% - {result["games_stats"]["suspended"]["hours"]}"
+            )
 
             total_percentage = (
                 sum(game["percentage"] for game in result["games_stats"]["games"])
@@ -64,7 +89,17 @@ class Plugin:
 
             return result
         except Exception as e:
-            print(f"Error: {e}")
+            print("Printing only the traceback above the current stack frame")
+            print(
+                "".join(
+                    traceback.format_exception(
+                        sys.exc_info()[0], sys.exc_info()[1], sys.exc_info()[2]
+                    )
+                )
+            )
+            print("")
+            print("Printing the full traceback as if we had not caught it here...")
+            print(format_exception(e))
 
 
 if __name__ == "__main__":
